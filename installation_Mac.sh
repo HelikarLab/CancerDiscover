3444e7edf950fec5a72caa9fe573f4f5e8fdfe05
Helikar Lab Personal <17307008+HelikarLabPersonal@users.noreply.github.com>
2017-11-15 22:11:14 -0600

    Update installation_Mac.sh

--- installation_Mac.sh
+++ installation_Mac.sh
@@ -25,6 +25,7 @@ brew install wget --with-libressl
 #Installing WEKA
 #This project utilizes WEKA 3-6-11. execute the following command:
 wget https://sourceforge.net/projects/weka/files/weka-3-6/3.6.11/weka-3-6-11.zip
+unzip weka-3-6-11.zip
 
 #Next, set the WEKA classpath by entering the following command in .bashrc file under Alias definitions:
 #export WEKAINSTALL=/absolute/path/to/weka/directory/`
