420077298be90479d7bcdb55094d27a75bafb934
Akram Mohammed <akram-mohammed@users.noreply.github.com>
2017-12-19 19:35:48 -0600

    Update installation_Linux.sh

--- installation_Linux.sh
+++ installation_Linux.sh
@@ -3,15 +3,15 @@
 #Before downloading CancerDiscover, make sure you have all the necessary software packages installed.
 #Downloading CancerDiscover
 #Clone the git repository:
-#git clone https://github.com/HelikarLab/CancerDiscover.git && cd CancerDiscover
-#sudo apt-get update
-#sudo apt-get install -y r-base wget gcc
-#sudo curl -L http://xrl.us/installperlnix | bash
+git clone https://github.com/HelikarLab/CancerDiscover.git && cd CancerDiscover
+sudo apt-get update
+sudo apt-get install -y r-base wget gcc
+sudo curl -L http://xrl.us/installperlnix | bash
 
 #Installing WEKA
 #This project utilizes WEKA 3-6-11. execute the following command:
 
-#wget https://sourceforge.net/projects/weka/files/weka-3-6/3.6.11/weka-3-6-11.zip
+wget https://sourceforge.net/projects/weka/files/weka-3-6/3.6.11/weka-3-6-11.zip
 
 #Next, set the WEKA classpath by entering the following command in .bashrc file under Alias definitions:
 #export WEKAINSTALL=/absolute/path/to/weka/directory/`
